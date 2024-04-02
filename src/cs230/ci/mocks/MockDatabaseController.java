47f3d32bbfda5f82c7c096a6f475eda7eed5007f
POHMANN001 <ohmann@cs.wisc.edu>
2024-04-02 09:36:47 -0500

    Add templates for Mock
    Again...just a starter...still work to do for those doing the exercise!

--- src/cs230/ci/mocks/MockDatabaseController.java
+++ src/cs230/ci/mocks/MockDatabaseController.java
@@ -1,6 +1,7 @@
 package cs230.ci.mocks;
 
 import cs230.ci.controllers.DatabaseController;
+import cs230.ci.entities.User;
 
 /**
  * A "mock" of the DatabaseController class.  Not yet implemented!
@@ -9,4 +10,9 @@ import cs230.ci.controllers.DatabaseController;
  */
 public class MockDatabaseController extends DatabaseController {
 
+	// add a user to the db
+	public static boolean addUser(User theUser) {
+		// what should this do?
+		return false;
+	}
 }
